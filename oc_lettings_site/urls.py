4ebb9b200c44ecdf5140ec32e9ff6fb95e5d95dd
Lahoucine <lbenmoulay@gmail.com>
2022-07-13 23:10:13 +0200

    Fix lint

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 
 from . import views
 
+
 def trigger_error(request):
     division_by_zero = 1 / 0
     return division_by_zero
+
 
 urlpatterns = [
     path('', views.index, name='index'),
