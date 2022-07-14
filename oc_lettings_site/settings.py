cb634a753c32902e593130bbef45b8e25bf6fd40
Lahoucine <lbenmoulay@gmail.com>
2022-07-14 04:26:42 +0200

    Update secret_key variables

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 try:
-    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', default='a_renseigner')
 except KeyError:
     SECRET_KEY = env('DJANGO_SECRET_KEY_LOCAL')
 
